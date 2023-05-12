393da0ae8ee68090cb8554b10173af17ae6d5809
dwagle <diwakar.wagle@gmail.com>
2023-05-12 16:29:27 +0545

    Fixed getOrders endpoint

--- routes/getOrder.js
+++ routes/getOrder.js
@@ -9,17 +9,20 @@ router.post("/orderHistory", async(req, res) => {
   try {
     let userData = await Order.findOne({"email": req.body.email});
     let resObj = [];
-    let orderInstance = []; 
-    userData.order_data.map((order) => {
-      orderInstance[0] = order[0].order_date;
+
+    if(userData) {
+    resObj = userData.order_data.map((order) => {
+      let orderInstance = []; 
+      orderInstance.push(order[0].Order_date);
       orderInstance.splice(1,0,...(order.slice(1)));
-      resObj.push(orderInstance);
+      return orderInstance;
     });
+    }
 
     res.json({orderData: resObj});
   } catch(error) {
     console.log(error.message);
-    res.send("Server Error", error.message);
+    res.send(500, error.message);
   }
 
 });
