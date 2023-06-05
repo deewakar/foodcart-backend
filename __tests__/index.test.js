c10fb7e6465e8dd0b878b569fa15d269266a7662
dwagle <diwakar.wagle@gmail.com>
2023-06-05 09:34:42 +0545

    added some more tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,3 +10,11 @@ describe('testing GET /', () => {
           .expect('Hello World!', done);
       });
 });
+
+describe('testing POST api/orderHistory', () => {
+  it('should fetch the order history', async () => {
+    const res = await request(app).post('/api/orderHistory')
+          .send({'email': 'vikas77@gmail.com'})
+          .expect(200)
+      });
+  });
